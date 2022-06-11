e9afe5a1183d0133068488ff541cbccac625d2de
Philani Ncube <ncbphi001@gmail.com>
2022-06-11 16:58:08 +0200

    hero padding

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="min-h-[calc(100vh-80px)] flex justify-center items-center">
+    <div className="min-h-[calc(100vh-80px)] flex px-4 justify-center items-center">
       <div className="max-w-7xl mx-auto py-4 px-6 md:px-4 relative min-h-[calc(100vh-80px)] w-full rounded-[50px] bg-hero bg-center bg-no-repeat bg-slate-500 bg-blend-multiply flex justify-start items-center shadow-2xl shadow-red-400/70">
         <h2 className="text-xl md:text-3xl lg:text-5xl text-white md:pl-20 font-bold">Bringing Consumers, <br /> <span className="text-red-600">Influencers</span> and Brands<br />   Together</h2>
 
